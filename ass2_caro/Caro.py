32b170f9da4dcb8d77d03cdc54d30ae1da259aa4
Thanh Nguyen <thanhnguyen2612@hcmut.edu.vn>
2021-05-10 14:06:28 +0700

    Finally have a pretty good EVAL function

--- ass2_caro/Caro.py
+++ ass2_caro/Caro.py
@@ -241,44 +241,63 @@ class CaroState:
             evaluation score of the pattern
         """
         if X not in repr and O not in repr: return 0
+
         def scorePattern(repr, pattern, player):
+            s, e = pattern.start(), pattern.end()
             if len(pattern.group(0)) < 5:
                 return 0
             elif len(pattern.group(0)) == 5:
-                s, e = pattern.start(), pattern.end()
                 if s > 0 and e < len(repr):
-                    if repr[s - 1] not in [player, EMPTY] and\
-                       repr[e] not in [player, EMPTY]:
-                       return 0
+                    if repr[s - 1] != player and repr[e] != player:
+                        return 0
+
+            # Score barem
+            consec_scores = (2, 5, 1000, 10000)
+            block_weight = (0.5, 0.6, 0.01, 0.25)
 
             p_str = pattern.group(0)
-            consec_scores = (2, 5, 1000, 10000)
             score, cons = 0, 0
+
+            # Count for number of consecutive
+            block = True
             for p in p_str:
                 if p != EMPTY:
                     cons += 1
                 elif cons > 0 and cons < 5:
-                    score += consec_scores[cons - 1]
+                    if block:
+                        score += consec_scores[cons - 1] * block_weight[cons - 1]
+                        block = False
+                    else:
+                        score += consec_scores[cons - 1]
                     cons = 0
                 else:
+                    block = False
                     cons = 0
 
-            if cons > 0 and cons < 4:
-                score += consec_scores[cons - 1]
+            # Last mark is PLAYER
+            if cons > 0 and cons < 5:
+                if e < len(repr):
+                    score += consec_scores[cons - 1] * block_weight[cons - 1]
+                else:
+                    score += consec_scores[cons - 1]
+
             return score
 
-        eval_score = 0
+        weight = (0.3, 0.7)     # Weight on current player
 
         # Score X
         X_ext = re.compile(r"[X.]+")
+        eval_X = 0
         for pattern in X_ext.finditer(repr):
-            eval_score += scorePattern(repr, pattern, X)
+            eval_X += scorePattern(repr, pattern, X)
         
         # Score O
         O_ext = re.compile(r"[O.]+")
+        eval_O = 0
         for pattern in O_ext.finditer(repr):
-            eval_score -= scorePattern(repr, pattern, O)
+            eval_O -= scorePattern(repr, pattern, O)
         
+        eval_score = eval_X * weight[self.player() == X] + eval_O * weight[self.player() == O]
         return eval_score
     
     def eval(self):
